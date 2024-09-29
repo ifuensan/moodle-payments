b3e323708402d9c78491e54ee8da394fa240884c
ifuensan <ibiko1@gmail.com>
2024-09-29 12:55:02 +0200

    Create enrol_lightning.php

---
+++ moodle/enrol/ligthning/lang/en/enrol_lightning.php
@@ -0,0 +1,31 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Languages configuration for the enrol_pluginname plugin.
+ *
+ * @package   enrol_pluginname
+ * @copyright Year, You Name <your@email.address>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['fee:config'] = 'Configure enrolment on payment enrol instances';
+$string['fee:manage'] = 'Manage enrolled users';
+$string['fee:unenrol'] = 'Unenrol users from course';
+$string['fee:unenrolself'] = 'Unenrol self from course';
+$string['pluginname'] = 'Enrolment on payment';
+$string['pluginname_desc'] = 'The enrolment on payment enrolment method allows you to set up courses requiring a payment. If the fee for any course is set to zero, then students are not asked to pay for entry. There is a site-wide fee that you set here as a default for the whole site and then a course setting that you can set for each course individually. The course fee overrides the site fee.';
+$string['privacy:metadata'] = 'The enrolment on payment enrolment plugin does not store any personal data.';
